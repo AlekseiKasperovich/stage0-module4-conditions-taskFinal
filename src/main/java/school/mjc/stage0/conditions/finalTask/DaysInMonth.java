41d5884398d9b0dab8c462866ed201d2df78b4a1
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-12 12:29:01 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,46 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
-    public void printDays(int year, int month) {
 
+    public void printDays(int year, int month) {
+        int days;
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                days = 31;
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                days = 30;
+                break;
+            case (2):
+                days = 28;
+                break;
+            default:
+                days = 0;
+                break;
+        }
+        boolean leap = false;
+        if (year % 4 == 0) {
+            leap = true;
+            if (year % 100 == 0 && year % 400 != 0) {
+                leap = false;
+            }
+        }
+        if (leap && month == 2) {
+            days = 29;
+        }
+        if (days == 0) {
+            System.out.println("invalid date");
+        } else {
+            System.out.println(days);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -1,6 +1,29 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
+
     public void printCompletelyDivided(int dividend, int divider) {
+        int a;
+        if (divider == 0) {
+            a = 0;
+        } else {
+            int res = dividend / divider;
+            if (res * divider == dividend) {
+                a = 1;
+            } else {
+                a = 2;
+            }
+        }
+        switch (a) {
+            case 0:
+                System.out.println("division by zero");
+                break;
+            case 1:
+                System.out.println("can be divided completely");
+                break;
+            case 2:
+                System.out.println("cannot be divided completely");
+                break;
+        }
     }
 }
